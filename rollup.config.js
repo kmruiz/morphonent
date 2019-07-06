6c880c93b82915b63ee02de67d5ce758bf732f82
Kevin Mas Ruiz <masruizkevin@gmail.com>
2019-07-07 00:50:38 +0200

    Fix rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,14 @@
-import babel from 'rollup-plugin-babel';
+import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
 
 export default {
-    input: 'lib/index.js',
-    output: {
-        file: 'dist/index.js',
-        format: 'esm'
-    },
-    plugins: [
-        babel(),
-        uglify()
-    ]
-};
+  input: 'lib/index.js',
+  output: {
+    file: 'dist/index.js',
+    format: 'esm'
+  },
+  plugins: [
+    babel(),
+    terser()
+  ]
+}
