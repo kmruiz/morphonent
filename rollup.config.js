4ccefb28a6aa1b2056d1428fc5f423b15b35de1c
Kevin Mas Ruiz <masruizkevin@gmail.com>
2020-04-06 19:06:03 +0200

    Upgrade dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -7,9 +7,9 @@
  */
 
 import babel from 'rollup-plugin-babel'
-import {terser} from 'rollup-plugin-terser'
-import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser'
+import commonjs from '@rollup/plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve'
 
 export default {
     input: 'lib/index.js',
